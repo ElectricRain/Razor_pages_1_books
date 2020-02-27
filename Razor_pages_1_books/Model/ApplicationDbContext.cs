8dd37aa2917a6b5bd2a10a1920f1a35050648264
ElectricRain <michalsokolowski1996@gmail.com>
2020-02-27 12:26:54 +0100

    Add database

---
+++ Razor_pages_1_books/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_pages_1_books.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        DbSet<Book> Book { get; set; }
+    }
+}
